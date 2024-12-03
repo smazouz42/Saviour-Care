09c8d88bb27530b5123d237c47cb4a7ca73fb56c
smazouz42 <smazouz@student.1337.ma>
2024-12-03 10:09:13 +0100

    feat: build a HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { HomePage } from "@/components/Home";
 import { NavBar } from "@/components/NavBar";
 
 export default function Home() {
   return (
-    <>
+    <div className="relative overflow-hidden h-screen">
       <NavBar />
-    </>
+      <HomePage />
+    </div>
   );
 }
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -15,7 +15,7 @@ const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div className="flex justify-between  p-8 container mx-auto">
-      <div className="flex items-center gap-2 container mx-auto ">
+      <div className="flex items-center gap-2  ">
         <Image
           src="/saviour-logo.png"
           alt="Vercel Logo"
@@ -25,7 +25,7 @@ const NavBar = () => {
         <h1 className="text-xl ">Saviour Care</h1>
       </div>
 
-      <div className="hidden md:flex items-center gap-10 container mx-auto">
+      <div className="hidden md:flex items-center gap-10 ">
         {NavItems.map((item) => (
           <a key={item} className="text-md text-gray-500 hover:border-b-2 border-blue-500 hover:text-blue-500 "  href={`#${item.toLowerCase()}`}>
             {item}
@@ -36,7 +36,7 @@ const NavBar = () => {
         <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
           <IoMdMenu className={'transform transition duration-500  ' + (isMenuOpen ? 'rotate-90 text-blue-500 ' : 'rotate-0')} size={30} />
         </button>
-          <div className={`absolute top-[100px] left-0 w-screen h-[calc(100vh-100px)] flex flex-col items-center justify-center gap-10  transition duration-500 ` + (isMenuOpen ? 'translate-x-0' : 'translate-x-full')}>
+          <div className={`absolute bg-white top-[100px] left-0 w-screen h-[calc(100vh-100px)] flex flex-col items-center justify-center gap-10  transition duration-500 ` + (isMenuOpen ? 'translate-x-0' : 'translate-x-full')}>
             {NavItems.map((item) => (
               <a key={item} className="text-md text-gray-500 hover:border-b-2 border-blue-500 hover:text-blue-500 "  href={`#${item.toLowerCase()}`}>
                 {item}
